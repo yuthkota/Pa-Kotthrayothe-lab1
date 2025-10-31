baa70928993173397a8a258e3efafe34e7d02f37
yuthkota <147136175+yuthkota@users.noreply.github.com>
2025-10-31 11:05:50 +0700

    Update greeting message again

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => {
-res.send('Hello, CI/CD!');
+res.send('Hello, GitHub Actions!');
 });
 module.exports = app;
